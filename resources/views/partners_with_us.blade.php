22b8c7f64f533261c705ec77bac5fa4352908af1
unknown <chaure.pankaj31@gmail.com>
2019-06-23 16:59:44 +0530

    updated

--- resources/views/partners_with_us.blade.php
+++ resources/views/partners_with_us.blade.php
@@ -5,102 +5,109 @@
   /* #overlay {position: fixed!important;width: 100%!important;height: 100%!important;top: 0!important;left: 0!important;right: 0!important;bottom: 0!important;background-color: rgba(0,0,0,0.5)!important; z-index: 2!important;display: block!important;}*/
     footer{margin-top: 0!important;}
     .clear{clear: both;}
+    .partner_page_h3{color:#fff;font-size: 30px;font-weight: 600;font-family: cursive;font-style: italic;line-height: 40px;letter-spacing: 2px;text-align: center;position: relative;top: 115px;}
+    .footer{padding-top: 0px!important;}
   </style>
   
   <!-- Slider -->
   <!-- bestsell Slider -->
-  <div class="col-lg-12 col-md-12" style="background: url(public/images/back1.jpg);">
+  <div class="col-lg-12 col-md-12" style="background: url(public/images/back1.jpg);padding: 0px;">
   
     <!-- <div id="overlay"> -->
-      <div class="col-lg-3 col-md-3"></div>
-      <div class="col-lg-6 col-md-6" style="background: #fff;margin: 50px 0px;padding: 0;">
-        <h2 style="background: #ccc;margin: 0;padding: 20px;color: #fff;text-align: center;font-weight: 600;">ADD RESTAURANT</h2>
-		 <?php if(Session::has('flash_message')): ?>
-                                   <div id="flashmsg" class="alert alert-success" role="alert" style="">
-                                  <?php echo e(Session::get('flash_message')); ?>
-                                  </div> 
-     <?php endif; ?>
-		<form id="frm1" method="post">
-        <div style="padding: 30px;">
-          <div class="col-lg-12 col-md-12" style="padding: 0px;">
-            <div class="col-lg-6 col-md-6" style="padding: 0;padding-right: 10px;">
-              <div class="form-group">
-                <input type="text" class="form-control" name="restaurant_name" placeholder="Restaurant Name" style="height: 46px;border-radius: 0;" required>
+      
+      <div class="col-lg-8 col-md-8" style="background: #fff;padding: 0;">
+        <h2 style="background: #040404;margin: 0;padding: 20px;color: #fff;text-align: center;font-weight: 600;">ADD RESTAURANT</h2>
+        <?php if(Session::has('flash_message')): ?>
+        <div id="flashmsg" class="alert alert-success" role="alert" style="">
+          <?php echo e(Session::get('flash_message')); ?>
+        </div> 
+        <?php endif; ?>
+        <form id="frm1" method="post">
+            <div style="padding: 93px 30px;">
+              <div class="col-lg-12 col-md-12" style="padding: 0px;">
+                <div class="col-lg-6 col-md-6" style="padding: 0;padding-right: 10px;">
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="restaurant_name" placeholder="Restaurant Name" style="height: 46px;border-radius: 0;" required>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6" style="padding: 0;padding-left: 10px;">
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="owner_name" placeholder="Owner Name" style="height: 46px;border-radius: 0;" required>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-6 col-md-6" style="padding: 0;padding-left: 10px;">
-              <div class="form-group">
-                <input type="text" class="form-control" name="owner_name" placeholder="Owner Name" style="height: 46px;border-radius: 0;" required>
+              <div class="col-lg-12 col-md-12" style="padding: 0px;">
+                <div class="col-lg-6 col-md-6" style="padding: 0;padding-right: 10px;">
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="contact_phone" placeholder="Phone No" style="height: 46px;border-radius: 0;" required>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6" style="padding: 0;padding-left: 10px;">
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="contact_email" placeholder="Email Id" style="height: 46px;border-radius: 0;"  required>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="col-lg-12 col-md-12" style="padding: 0px;">
-            <div class="col-lg-6 col-md-6" style="padding: 0;padding-right: 10px;">
-              <div class="form-group">
-                <input type="text" class="form-control" name="contact_phone" placeholder="Phone No" style="height: 46px;border-radius: 0;" required>
+              <div class="col-lg-12 col-md-12" style="padding: 0px;">
+                <div class="col-lg-6 col-md-6" style="padding: 0;padding-right: 10px;">
+                  <div class="form-group">
+                    <select class="form-control" name="city" style="height: 46px;border-radius: 0;" required="true">
+                      <option value="">Select City</option>
+                      <option value="Nagpur">Nagpur</option>
+                      <option value="Pune">Pune</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6" style="padding: 0;padding-left: 10px;">
+                  <div class="form-group">
+                    <select class="form-control" name="state" style="height: 46px;border-radius: 0;" required="true">
+                      <option>Select State</option>
+               <option value="Maharashtra">Maharashtra</option>
+                    
+                    </select>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-6 col-md-6" style="padding: 0;padding-left: 10px;">
-              <div class="form-group">
-                <input type="text" class="form-control" name="contact_email" placeholder="Email Id" style="height: 46px;border-radius: 0;"  required>
+              <div class="col-lg-12 col-md-12" style="padding: 0px;">
+                <div class="col-lg-6 col-md-6" style="padding: 0;padding-right: 10px;">
+                  <div class="form-group">
+                     <input type="text" class="form-control" name="address" placeholder="Location" style="height: 46px;border-radius: 0;" required>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6" style="padding: 0;padding-left: 10px;">
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="post_code" placeholder="Zipcode" style="height: 46px;border-radius: 0;" required>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="col-lg-12 col-md-12" style="padding: 0px;">
-            <div class="col-lg-6 col-md-6" style="padding: 0;padding-right: 10px;">
-              <div class="form-group">
-                <select class="form-control" name="city" style="height: 46px;border-radius: 0;" required="true">
-                  <option value="">Select City</option>
-                  <option value="Nagpur">Nagpur</option>
-                  <option value="Pune">Pune</option>
-                </select>
+              <div class="col-lg-12 col-md-12" style="padding: 0px;">
+                <div class="col-lg-6 col-md-6" style="padding: 0;padding-right: 10px;">
+                  <div class="form-group">
+                    <input type="password" class="form-control" name="password" placeholder="Password" style="height: 46px;border-radius: 0;" required>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6" style="padding: 0;padding-left: 10px;">
+                  <div class="form-group">
+                    <input type="password" class="form-control" name="c_password" placeholder="Confirm Password" style="height: 46px;border-radius: 0;" required>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-6 col-md-6" style="padding: 0;padding-left: 10px;">
-              <div class="form-group">
-                <select class="form-control" name="state" style="height: 46px;border-radius: 0;" required="true">
-                  <option>Select State</option>
-				   <option value="Maharashtra">Maharashtra</option>
-                
-                </select>
+              <div class="col-lg-12 col-md-12" style="padding: 0px;">
+                <input type="checkbox" class="form-control" name="chk" style="width: 4%;float: left;margin: 0;padding: 0;height: 18px;" required="true"><span>I Agree To The Terms And Conditions And Privacy Policy</span>
               </div>
-            </div>
-          </div>
-          <div class="col-lg-12 col-md-12" style="padding: 0px;">
-            <div class="col-lg-6 col-md-6" style="padding: 0;padding-right: 10px;">
-              <div class="form-group">
-                 <input type="text" class="form-control" name="address" placeholder="Location" style="height: 46px;border-radius: 0;" required>
+              <div class="col-lg-12 col-md-12" style="padding: 0px;">
+                <button class="btn btn-success" style="float: right;">Submit</button>
               </div>
+              <div class="clear"></div>
             </div>
-            <div class="col-lg-6 col-md-6" style="padding: 0;padding-left: 10px;">
-              <div class="form-group">
-                <input type="text" class="form-control" name="post_code" placeholder="Zipcode" style="height: 46px;border-radius: 0;" required>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-12 col-md-12" style="padding: 0px;">
-            <div class="col-lg-6 col-md-6" style="padding: 0;padding-right: 10px;">
-              <div class="form-group">
-                <input type="password" class="form-control" name="password" placeholder="Password" style="height: 46px;border-radius: 0;" required>
-              </div>
-            </div>
-            <div class="col-lg-6 col-md-6" style="padding: 0;padding-left: 10px;">
-              <div class="form-group">
-                <input type="password" class="form-control" name="c_password" placeholder="Confirm Password" style="height: 46px;border-radius: 0;" required>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-12 col-md-12" style="padding: 0px;">
-            <input type="checkbox" class="form-control" name="chk" style="width: 4%;float: left;margin: 0;padding: 0;height: 18px;" required="true"><span>I Agree To The Terms And Conditions And Privacy Policy</span>
-          </div>
-          <div class="col-lg-12 col-md-12" style="padding: 0px;">
-            <button class="btn btn-success" style="float: right;">Submit</button>
-          </div>
-          <div class="clear"></div>
-        </div>
-		</form>
-		
+        </form>
+    
       </div>
+      <div class="col-lg-4 col-md-4" style="padding: 0px;background: #3a126c;">
+        <center><img src="uploads/rabzo_circle_logo.png" style="width: 100px;position: relative;top: 60px;"></center>
+        <h3 class="partner_page_h3">Connect With Us And Growup Your Bussiness !</h3>
+        <center><img src="uploads/delivery_scooter1.png" style="width: 448px;margin-top: 210px;"></center>
+      </div> 
     <!-- </div> -->
   </div>
 @include('footer')
@@ -128,10 +135,10 @@ function off() {
 <script>
 /*function getMerchantData()
 {
-	getMerchant();
+  getMerchant();
 }*/
 $(document).ready(function(){
-	getMerchant();
+  getMerchant();
   //alert('hi')
 })
 
@@ -171,4 +178,4 @@ $("#frm1").submit(function(e){
  $('#flashmsg').fadeOut();
  $('#flashmsg').val('')
 }, 1000 );
-		</script>
+    </script>
--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -32,6 +32,31 @@
     .cuisine_ul{padding: 0;margin: 0px;}
     .cuisine_ul li{list-style-type: none;padding: 6px 11px;border: 1px solid #d8d6d670;background: #cccccc42;}
     .cuisine_ul li a{color: #000;}
+    .restaurant_page_div{margin-top: 40px;}{}
+    .restaurant_page_search_div{position: sticky;top: 0;z-index: 99;padding: 6px 0px;}
+    .restaurant_search_button_div input{background: #3a126c;color: #fff;border-radius: 0;border: none;padding: 6PX 20px;}
+    .restaurant_location_icon{color: #fff;position: relative;float: right;top: -27px;right: 15px;font-size: 19px;}
+    .restaurant_dishesh_icon{color: #fff;position: relative;float: right;top: -27px;right: 15px;font-size: 19px;}
+    .restaurant_category_title{color: #fff;font-size: 15px;}
+    .restaurant_page_restaurant_img{height: 160px;width: 100%;}
+    .restaurant_feature_images{width: 100%;border-radius: 4px;margin-bottom: 10px;height: 84px;}
+    .restaurant_nearby_restaurant_img{width: 100%;border-radius: 4px;margin-bottom: 10px;height: 84px;}
+    @media(min-width: 320px) and (max-width: 479px)
+    {
+      .wrap{margin-top: 50px;height: 760px;}
+      .search{width: 380px;height: 45px;top: -61px!important;}
+      .restaurant_page_div{margin-top: 70px;}
+      .restaurant_page_search_div{height: 52px;}
+      .fix-search .search{top: 0!important;width: 82%;left: 58px;}
+      .restaurant_search_button_div input{padding: 6PX 12px;}
+      .restaurant_location_icon{color: #000;}
+      .restaurant_dishesh_icon{color: #000;}
+      .restaurant_category_title{padding:10px;}
+      .restaurant_page_restaurant_div{padding: 0px;margin-bottom: 30px;}
+      .restaurant_page_restaurant_img{height: 200px;}
+      .restaurant_feature_images{height: 180px;}
+      .restaurant_nearby_restaurant_img{height: 180px;}
+    }
   </style>
   
   <!-- Slider -->
@@ -46,7 +71,7 @@
               <h4>Filters</h4>
             </div> -->
             <div class="col-lg-12 col-md-12" style="background: #3a126c;">
-              <h4 style="color: #fff;font-size: 15px;">Category</h4>
+              <h4 class="restaurant_category_title" style="">Category</h4>
             </div>
             <div class="col-lg-12 col-md-12" style="padding-top: 10px;">
              @foreach($results['category'] as $cat)
@@ -58,10 +83,10 @@
 				          @endforeach 
              
             </div>
-            <div class="col-lg-12 col-md-12" style="background: #3a126c;">
+            <div class="col-lg-12 col-md-12 restaurant_page_cuisine_title" style="background: #3a126c;">
               <h4 style="color: #fff;font-size: 15px;">Cuisine</h4>
             </div>
-            <div class="col-lg-12 col-md-12" style="padding: 0;">
+            <div class="col-lg-12 col-md-12 restaurant_page_cuisine_div" style="padding: 0;">
                <ul class="cuisine_ul">
 							@foreach($results['cuisine'] as $cu)
 				                <li><a href="javascript:void(0)" onClick="getMerchant(<?php echo $cu->id ?>)"><i class="fa fa-caret-right"></i> {{$cu->cuisine_name}}</a></li>
@@ -70,24 +95,25 @@
             </div>
           </div>
           <div class="col-lg-7 col-md-7" style="padding: 0px;" >
-            <div class="col-lg-12 col-md-12" style="padding: 0px;">
+            <div class="col-lg-12 col-md-12" style="padding: 0px; ">
               <!--search div-->
               <div class="search">
-                <div class="col-lg-12 col-md-12" id="search_navbar" style="position: sticky;top: 0;z-index: 99;padding: 6px 0px;">
-                  <div class="col-lg-4 col-md-4" style="padding-right: 0px;">
-                    <input type="text" id="location" placeholder="Search location" class="form-control" name="" style="border: 1px solid #ccc;color: #000;border-radius: 0;"  value="<?php if(isset($_GET['location'])){ echo $_GET['location']; } ?>">
-                    <i class="fa fa-search" style="color: #fff;position: relative;float: right;top: -27px;right: 15px;font-size: 19px;"></i>
+                <div class="col-lg-12 col-md-12 restaurant_page_search_div" id="search_navbar" style="">
+                  <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 restaurant_search_location_div" style="padding-right: 0px;">
+                    <input type="text" id="location" placeholder="location" class="form-control" name="" style="border: 1px solid #ccc;color: #000;border-radius: 0;"  value="<?php if(isset($_GET['location'])){ echo $_GET['location']; } ?>">
+                    <i class="fa fa-crosshairs restaurant_location_icon" style=""></i>
                   </div>
-                  <div class="col-lg-6 col-md-6" style="padding-right: 0px;">
-                    <input type="text" id="category" placeholder="Search for" class="form-control" name="" style="border: 1px solid #ccc;color: #000;border-radius: 0;"  value="<?php if(isset($_GET['category'])){ echo $_GET['category']; } ?>">
-                    <i class="fa fa-map-marker" style="color: #fff;position: relative;float: right;top: -27px;right: 15px;font-size: 19px;"></i>
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 restaurant_search_dishesh_div" style="padding-right: 0px;">
+                    <input type="text" id="category" placeholder="Restaurant/Dishes" class="form-control" name="" style="border: 1px solid #ccc;color: #000;border-radius: 0;"  value="<?php if(isset($_GET['category'])){ echo $_GET['category']; } ?>">
+                    <i class="fa fa-search restaurant_dishesh_icon" style=""></i>
                   </div>
-                  <div class="col-lg-2 col-md-2" style="padding:0px;">
-                    <input type="button" placeholder="Search for" class="btn btn-default" name="" value="Search" style="background: #3a126c;color: #fff;border-radius: 0;border: none;padding: 6PX 20px;">
+                  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 restaurant_search_button_div" style="padding:0px;">
+                    <input type="button" placeholder="Search for" class="btn btn-default" name="" value="Search" >
                   </div>
+                  <div class="clear"></div>
               </div>
               </div>
-              <div id="contents" style="margin-top:40px;">
+              <div id="contents" class="restaurant_page_div">
               </div>
               <!-- <div class="col-lg-12 col-md-12" id="search_navbar" style="background: #fff;position: sticky;top: 0;z-index: 99;padding: 15px 0px 0px;">
                 <div class="col-lg-6 col-md-6" style="padding-right: 0px;">
@@ -102,6 +128,7 @@
                   <input type="button" onClick="getMerchant()" placeholder="Search for" class="btn btn-default" name="" value="Search" style="background: #3a126c;color: #fff;border-radius: 0;border: none;padding: 7PX 53px;">
                 </div>
               </div> -->
+              <div class="clear"></div>
             </div>
            
           </div>
@@ -115,7 +142,7 @@
             <div class="col-lg-12 col-md-12" style="padding:15px 0px 0px 0px;background: #fff;">
 		          @foreach($results['featured'] as $row)
               <div class="col-lg-6 col-md-6" style="">
-                <img src="{{ asset('uploads/').'/'.$row->images }}" style="width: 100%;border-radius: 4px;margin-bottom: 10px;height: 84px;" />
+                <img src="{{ asset('uploads/').'/'.$row->images }}" class="restaurant_feature_images" />
                 <p style="font-weight: 600;margin: 0px;">{{$row->restaurant_name}}</p>
                 <p><?php echo $row->address; ?>, <?php echo $row->city; ?></p>
               </div>
@@ -127,7 +154,7 @@
             </div>
             <div class="col-lg-12 col-md-12" style="padding:15px 0px 0px 0px;background: #fff;">
               <div class="col-lg-6 col-md-6" style="">
-                <img src="images/food1.jpg" style="width: 100%;border-radius: 4px;margin-bottom: 10px;height: 84px;">
+                <img src="images/food1.jpg" class="restaurant_nearby_restaurant_img">
                 <p>Enjoy Live Sports Screening With Special Offers !</p>
               </div>
               <div class="col-lg-6 col-md-6" style="">
@@ -200,7 +227,7 @@ function getMerchant($id='')
 				  var div1 ='';
 				  $.each(res, function(key,val){
 				// alert(val.item_name)
-				  div1 += ' <div class="col-lg-4 col-md-4"><div class="" style="border: none;height: 285px;    margin-top: 16px;"> <div class="" style="padding: 0px;"> <div class="cart">';
+				  div1 += ' <div class="col-lg-4 col-md-4 restaurant_page_restaurant_div"><div class="" style="border: none;height: 285px;margin-top: 16px;"> <div class="" style="padding: 0px;"> <div class="cart">';
 				  if(val.offer > 0){
 				 div1 +='<div class="ribbon"> <h2 class="header">Save '+val.offer+' %</h2> </div>';
 				  }
@@ -217,7 +244,7 @@ function getMerchant($id='')
 				  else{
 					  var rating ='';
 				  }
-				 div1 +='</div> <img src="uploads/'+val.images+'" class="image" style="height: 160px;width: 100%;"> '+offfer_per+rating+'<a href="{{url('restaurant_details')}}/'+val.restaurant_name+'?location='+location+'&category='+category+'"><div style="background:#fff;padding: 0px 10px 10px 9px;"><h3 class="restaurant_details_div_title" style="top: 0px;color: #000;">'+val.restaurant_name+'</h3><p style="color: #000;">'+val.address+'</p><p style="color: #000;">Distance - 30 Min</p></div></a> </div> </div> </div>';
+				 div1 +='</div> <img src="uploads/'+val.images+'" class="image restaurant_page_restaurant_img"> '+offfer_per+rating+'<a href="{{url('restaurant_details')}}/'+val.restaurant_name+'?location='+location+'&category='+category+'"><div style="background:#fff;padding: 0px 10px 10px 9px;"><h3 class="restaurant_details_div_title" style="top: 0px;color: #000;">'+val.restaurant_name+'</h3><p style="color: #000;">'+val.address+'</p><p style="color: #000;">Distance - 30 Min</p></div></a> </div> </div> </div>';
 				  });
 				  //alert(div1)
 				   $("#contents").html(div1);
